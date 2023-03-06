0b8706ebb0540fbcf447d2f8ed7c8b5aee22fb5c
João Rios <joao.rios@compasso.com.br>
2023-03-06 09:23:00 -0300

    Tela de cesta e confirmação de compra

--- src/telas/Produtores/index.js
+++ src/telas/Produtores/index.js
@@ -5,16 +5,21 @@ import Produtor from './componentes/Produtor';
 import Topo from './componentes/Topo';
 import useProdutores from '../../hooks/useProdutores';
 import useTextos from '../../hooks/useTextos';
-import { useNavigation } from '@react-navigation/native';
+import { useNavigation, useRoute } from '@react-navigation/native';
 
 export default function Produtores({ melhoresProdutores }) {
   const navigation = useNavigation();
+  const route = useRoute();
   const lista = useProdutores(melhoresProdutores);
-  const { tituloProdutores } = useTextos();
-
+  const { tituloProdutores, mensagemCompra } = useTextos();
+  
+  const nomeCompra = route.params?.compra.nome;
+  const mensagemCompleta = mensagemCompra?.replace('$NOME', nomeCompra);
+  
   const TopoLista = () => {
     return <>
       <Topo melhoresProdutores={melhoresProdutores} />
+      { !!nomeCompra && <Text style={estilos.compra}>{mensagemCompleta}</Text> }
       <Text style={estilos.titulo}>{tituloProdutores}</Text>
     </>
   }
@@ -40,11 +45,18 @@ const estilos = StyleSheet.create({
     backgroundColor: '#ffffff',
   },
   titulo: {
+    color: '#464646',
     fontSize: 20,
+    fontWeight: 'bold',
     lineHeight: 32,
     marginHorizontal: 16,
     marginTop: 16,
-    fontWeight: 'bold',
+  },
+  compra: {
+    backgroundColor: '#EAF5F3',
     color: '#464646',
+    fontSize: 16,
+    lineHeight: 26,
+    padding: 16,
   }
 })
